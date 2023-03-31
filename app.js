a968389b3ec1c793441c8002f7ca5b85cf465e20
JJJUhlar <jjuhlar@gmail.com>
2023-03-31 11:01:24 +0100

    added socket io outline

--- app.js
+++ app.js
@@ -17,8 +17,39 @@ const {
 } = require('./controllers/controllers.js')
 const { handle404s } = require('./controllers/errorHandlingControllers')
 
+// socketio setup
+const http = require('http')
+const { Server } = require('socket.io')
 
 app.use(cors());
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket)=>{
+    console.log(`User connected: ${socket.id}`)
+
+    socket.on('join_room', (data)=>{
+        const {user, room} = data;
+        socket.join(room);
+        console.log(`${user} joined ${room}`)    
+    })
+
+    socket.on('send_message', (data)=>{
+        console.log(data, `got message`)
+        const {room, msg} = data;
+        socket.in(room).emit('send_message', data)
+    })
+})
+server.listen(4040, ()=>{
+    console.log('listening on port 4040')
+})
+
+
 app.use(express.json())
 
 app.post('/api/users', postUser)
