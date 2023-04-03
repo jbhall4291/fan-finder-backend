e984f9d7b6e3c0deb4f2871ce992ca6a7b38fc49
JJJUhlar <jjuhlar@gmail.com>
2023-04-03 10:14:09 +0100

    fixed runSeed call

--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
 const app = require ('./app.js')
 const { PORT = 9090 } = process.env;
-const {runseed} = require('./db/runseed')
+const runSeed = require('./db/runseed')
 
-runseed()
+runSeed()
 
 app.listen(PORT, () => {console.log(`listening on port ${PORT}`)})
